dadd9fb81950699d1ef26cad617acc26c4b3860d
dellyu03 <seunghwan081@gmail.com>
2024-10-13 18:19:33 +0900

    서비스 개발

--- src/main/kotlin/com/seunghwan/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/seunghwan/portfolio/presentation/service/PresentationService.kt
@@ -1,4 +1,52 @@
 package com.seunghwan.portfolio.presentation.service
 
-class PresentationService {
+import com.seunghwan.portfolio.domain.entity.Introduction
+import com.seunghwan.portfolio.presentation.dto.IntroductionDTO
+import com.seunghwan.portfolio.presentation.dto.LinkDTO
+import com.seunghwan.portfolio.presentation.dto.ProjectDTO
+import com.seunghwan.portfolio.presentation.dto.ResumeDTO
+import com.seunghwan.portfolio.presentation.repository.PresentationRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class PresentationService(
+    private val presentationRepository: PresentationRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun getIntroductions(): List<IntroductionDTO> {
+        val introductions = presentationRepository.getActiveIntroductions()
+
+        return introductions.map { IntroductionDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks(): List<LinkDTO> {
+        val links = presentationRepository.getActiveLinks()
+
+        return links.map { LinkDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getResume(): ResumeDTO {
+
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+
+        return ResumeDTO(
+            experiences = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+
+    @Transactional(readOnly = true)
+    fun getProjects(): List<ProjectDTO> {
+        val projects = presentationRepository.getActiveProjects()
+
+        return projects.map { ProjectDTO(it) }
+    }
+
 }
